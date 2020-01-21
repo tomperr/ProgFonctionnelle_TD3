baf1ae3a0eb5e3c098058233081d412214223eb0
Tom PERRILLAT-COLLOMB <perrilto@iut-acy.local>
2020-01-21 09:46:16 +0100

    update code

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -4,8 +4,8 @@ object Main {
 
    def main(args: Array[String]) {
         println("Pascal's Triangle")
-        for (row ‚Üê 0 to 10) {
-            for (col ‚Üê 0 to row)
+        for (row <- 0 to 10) {
+            for (col <- 0 to row)
                 print(coefBinomial(col, row) + " ")
             println()
         }
@@ -15,27 +15,69 @@ object Main {
      * Exercise 1
      * Triangle de Pascal
      */
-    def coefBinomial(col: Int, ligne: Int): Int = ???
-
+    def coefBinomial(col: Int, ligne: Int): Int = {
+      (col, ligne) match {
+        case (_,_) if (col > ligne || col < 0 || ligne < 0) => throw new IllegalArgumentException("ParamËtres invalides") 
+        case (0, _) => 1
+        case (_, _) if col == ligne => 1
+        case (_, _) => coefBinomial(col, ligne-1) + coefBinomial(col-1, ligne-1)
+      }
+    }
+    
     /**
      * Exercise 2
      * √âcrire une fonction r√©cursive qui renvoie vraie ssi une liste de caract√®res est un palindrome
      */
-    def isPalindrome(list: List[Char]): Boolean = ???
-
-    /**
+    
+    def isPalindrome(list: List[Char]): Boolean = {
+      def renverse(myList: List[Char]): List[Char] = {
+        myList match {
+          case Nil => myList
+          case elem::Nil => elem::Nil
+          case _ => myList.last::renverse(myList.init)
+        }
+      }
+      
+      list == renverse(list)
+    }    
+    
+     /**
      * Exercise 3
-     * Soit, d'apr√®s B.Werber, la suite d√©butant par 1 et telle que chaque terme soit l'√©num√©ration du terme  pr√©c√©dent,  √©num√©ration  constitu√©e  simplement  par
-     * le  nombre  de  fois  qu'un  chiffre  se trouve plusieurs fois √©crit cons√©cutivement. Ainsi √† l'√©tape 111221, la lecture de gauche √† droite de ce "mot" sera
-     * 3 fois le chiffre 1 puis 2fois le chiffre 2 et enfin 1 fois le chiffre 1. L'√©tape suivante sera donc constitu√©e par 312211.
+     * Soit, d'aprËs B.Werber, la suite dÈbutant par 1 et telle que chaque terme soit l'ÈnumÈration du terme  prÈcÈdent,  ÈnumÈration  constituÈe  simplement  par
+     * le  nombre  de  fois  qu'un  chiffre  se trouve plusieurs fois Ècrit consÈcutivement. Ainsi ‡ l'Ètape 111221, la lecture de gauche ‡ droite de ce "mot" sera
+     * 3 fois le chiffre 1 puis 2fois le chiffre 2 et enfin 1 fois le chiffre 1. L'Ètape suivante sera donc constituÈe par 312211.
      */
-    def ant(etape: List[Int], nombreIteration: Int): List[Int] = ???
+    def ant(etape: List[Int], nombreIteration: Int): List[Int] = {
+      
+      // one iteration
+      def once(_etape: List[Int], previous: Int, count: Int) : List[Int] = {
+        _etape match {
+          case Nil => Nil
+          case elem::reste if elem == previous => once(reste, elem, count+1) 
+          case elem::reste if elem != previous => 
+            count::previous::once(reste, elem, 1)
+        }
+      }
+    }
 
     /**
      * Exercise 4
      * √âquilibrage des parenth√®ses dans une cha√Æne de caract√®res.
      */
-    def equili(chars: List[Char]): Boolean = ???
+    def equili(chars: List[Char]): Boolean = {
+      def _balance(myList: List[Char], opened: Long): Boolean = {
+        (myList, opened) match {
+          case (_, -1) => false
+          case (elem::reste,_) if elem == '(' => _balance(reste, opened+1)
+          case (elem::reste,_) if elem == ')' => _balance(reste, opened-1)
+          case (elem::reste,_) => _balance(reste, opened)
+          case (Nil, _) if opened == 0 => true
+          case (Nil, _) if opened != 0 => false
+        }
+      }
+      
+      _balance(chars, 0) 
+    }
 
     /**
      * Exercise 5
@@ -56,8 +98,7 @@ object Main {
      * d‚Äôune liste de valeurs de billets disponibles. Nous supposerons que cette liste est donn√©e de fa√ßon d√©croissante.
      * Par exemple : change (List(5, 10, 20, 50, 100, 200, 500),1790 ) = 1602321
      */
+    
     def nombreDeChange(monnaies: List[Int], montant: Int): Int = ???
-
-
 
 }
